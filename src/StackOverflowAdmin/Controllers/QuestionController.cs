f3085d26d11638dc9c6fec08f4812ce6e04b87b9
Andrew Cho <cho-andrew@hotmail.com>
2016-04-26 09:48:59 -0700

    add fucntionality to show questions of specificed users

---
+++ src/StackOverflowAdmin/Controllers/QuestionController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Security.Claims;
+using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Authorization;
+using Microsoft.AspNet.Identity;
+using StackOverflowAdmin.Models;
+
+namespace StackOverflowAdmin.Controllers
+{
+    public class QuestionController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public QuestionController(
+            UserManager<ApplicationUser> userManager,
+            ApplicationDbContext db
+            )
+        {
+            _userManager = userManager;
+            _db = db;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var currentUser = await _userManager.FindByIdAsync(User.GetUserId());
+            return View(_db.Questions.Where(x => x.User.Id == currentUser.Id));
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Question question)
+        {
+            var currentUser = await _userManager.FindByIdAsync(User.GetUserId());
+            question.User = currentUser;
+            _db.Questions.Add(question);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
